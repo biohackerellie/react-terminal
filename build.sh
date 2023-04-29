9c8a9a173495bfbe185c028145e60488b042f295
biohackerellie <epkerns@gmail.com>
2023-04-29 10:34:24 -0600

    updated build script and added docker-compose

--- build.sh
+++ build.sh
@@ -1,32 +1,28 @@
 #!/bin/bash
 
-#we make 2 variables at the start, this allows this script to be easier to copy and paste for different containers with less retyping
-CONTAINER_NAME=terminal
-IMAGE_NAME=terminal
+cd /apps/react-terminal
 
-# Change to the directory containing the Git repository
-cd apps/react-terminal
-
-# Check for updates
-git fetch
-
-# Compare local and remote branches to check for changes
-if ! git diff --quiet HEAD @{u}; then
-    # Pull changes if there are any
-    git pull && \
-
-    # Rebuild the image from Dockerfile
-    docker build -t $IMAGE_NAME:latest . && \
-    # Stop docker container if its running
-    if docker container inspect $CONTAINER_NAME >/dev/null 2>&1; then
-        echo "$CONTAINER_NAME is running, shutting down" && \
-        docker container stop $CONTAINER_NAME
-    fi
-    # Deletes old container
-    docker system prune -f && \
-    # Restart container with new image
-    docker run --rm --name $CONTAINER_NAME -p 4020:4020 -d $IMAGE_NAME:latest
-
+# Get the name of the service that has changes
+if git diff --quiet HEAD..HEAD~ -- ./server; then
+  service=server
+elif git diff --quiet HEAD..HEAD~ -- ./front-end; then
+  service=frontend
 else
-    echo "No changes. Exiting."
+  service=all
 fi
+
+# Update the repository to the latest changes
+git fetch origin main
+git checkout main
+git pull origin main
+
+# Rebuild the Docker container for the specified service
+if [ "$service" == "all" ]; then
+  docker-compose build
+else
+  docker-compose build $service
+fi
+
+# Restart the Docker containers
+docker-compose down
+docker-compose up -d
